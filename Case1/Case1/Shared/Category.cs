39ff5d1ae41721f1427c931ba6d23481ef88402d
biproberkay <biproberkay@gmail.com>
2022-08-27 16:40:37 +0300

    first step

---
+++ Case1/Case1/Shared/Category.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Case1.Shared
+{
+    //https://stackoverflow.com/a/64401612/12878692
+    public class Category
+    {
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+        public string Title { get; set; }
+    }
+
+    public static class CategoryHelpers
+    {
+        public static IEnumerable<Category> Categories = new[]
+        {
+        new Category { ParentId = 0, Id = 1, Title = "Gre" },
+        new Category { ParentId = 0, Id = 2, Title = "GMATE" },
+        new Category { ParentId = 1, Id = 4, Title = "Quant" },
+        new Category { ParentId = 1, Id = 9, Title = "Verbal" },
+        new Category { ParentId = 2, Id = 3, Title = "Quant" },
+        new Category { ParentId = 2, Id = 12, Title = "Verbal" },
+        new Category { ParentId = 3, Id = 11, Title = "Algebra" },
+        new Category { ParentId = 4, Id = 10, Title = "Profit" },
+    };
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # biproblazorcases
 i am developing an application with blazor. i repeatedly face some issues at the developing proccess. so i want to start a repository includes projects based on atomic cases. let's start 
+
+## Why projects must be atomic?
+because issues more than one could be a little confusing.
+
+## Cases
+### 1. case1: 
+i have two component. one for to list a file or object(foo) data. one for categories of the foo. 
+#### what i want?
+i want to click on the category link and the second component filter and list the related data.
